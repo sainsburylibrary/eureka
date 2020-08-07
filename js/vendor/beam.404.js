8ceaafa88a71c6d94bd7b92dbb7a0cfb0becb93b
johnpilbeam <jpilbeam@gmail.com>
2020-08-07 11:27:25 +0100

    Fixed noscript issue

--- js/vendor/beam.404.js
+++ js/vendor/beam.404.js
@@ -3,9 +3,7 @@ window.onload = function() {
 var LastCaptureURL = 'https://wayback.archive-it.org/2502/20190705110514/';  
 var CurrentPageURL = window.location.href;  
 var RedirectURL = LastCaptureURL + CurrentPageURL;
-var RedirectOptions = '<ul><li><strong><a id="beamlink" href="">View an archived version in the Bodleian Libraries&rsquo; Web Archive</a></strong></li><li><a href="https://wayback.archive-it.org/2502/*/http://eureka.sbs.ox.ac.uk/">Browse the Bodleian Libraries&rsquo; Web Archive of Eureka</a></li></ul>'
 
 // Write to document
-	document.getElementById("beamoptions").innerHTML = RedirectOptions;
 	document.getElementById("beamlink").setAttribute("href",RedirectURL);	
 }
