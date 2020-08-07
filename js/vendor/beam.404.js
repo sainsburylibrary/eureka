002b32417510a8d2c452a594360e6a11ec478590
johnpilbeam <jpilbeam@gmail.com>
2020-08-07 09:47:58 +0100

    Updated 404

--- js/vendor/beam.404.js
+++ js/vendor/beam.404.js
@@ -1,9 +1,9 @@
-(function() {
-	// Set the variables
-	var LastCaptureURL = 'https://wayback.archive-it.org/2502/20190705110514/';  
-	var CurrentPageURL = window.location.href;  
-	var RedirectURL = LastCaptureURL + CurrentPageURL;
+window.onload = function() {
+// Set the variables
+var LastCaptureURL = 'https://wayback.archive-it.org/2502/20190705110514/';  
+var CurrentPageURL = window.location.href;  
+var RedirectURL = LastCaptureURL + CurrentPageURL;
 
-	// Write to document
+// Write to document
 	document.getElementById("beamlink").setAttribute("href",RedirectURL);	
 }
