090eaba8e6fd0cf9210e884cfd1839e41a3b2ad7
johnpilbeam <jpilbeam@gmail.com>
2020-08-27 10:16:18 +0100

    Pre-launch updates

--- js/vendor/beam.404.js
+++ js/vendor/beam.404.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 // Set the variables
-var LastCaptureURL = 'https://wayback.archive-it.org/org-467/20190925162139/';  
+var LastCaptureURL = 'https://wayback.archive-it.org/org-467/20200807110009/';  
 var CurrentPageURL = window.location.href;  
 var RedirectURL = LastCaptureURL + CurrentPageURL;
 
